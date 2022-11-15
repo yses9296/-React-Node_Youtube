03c5e851c6653233be64a269fe01862d0f708f16
Eunseo Choi <yses9296@gmail.com>
2022-11-15 21:28:50 +0900

    Node React 13

--- node-react-youtube/client/src/components/views/SubscriptionPage/SubscriptionPage.js
+++ node-react-youtube/client/src/components/views/SubscriptionPage/SubscriptionPage.js
@@ -59,7 +59,7 @@ function SubscriptionPage() {
 
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
-            <Title level={2} > Recommended </Title>
+            <Title level={2}> Subscription </Title>
             <hr />
 
             <Row gutter={[32, 16]}>

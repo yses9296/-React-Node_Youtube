9cd23b00b02455cc6b4d64e798147c672882dbd9
Eunseo Choi <yses9296@gmail.com>
2022-11-15 21:44:38 +0900

    Node React 14

--- node-react-youtube/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ node-react-youtube/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
 import SideVideo from './Sections/SideVideo';
 import Subscribe from './Sections/Subscribe';
+import Comment from './Sections/Comment'
 
 function VideoDetailPage(props) {
 
@@ -29,13 +30,17 @@ function VideoDetailPage(props) {
 
     if (VideoDetail.writer) {
 
+        //업로드한 사용자가 자기 자신을 구독할 수 없도록.
+        const subscribeBtn = VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>
+
         return (
             <Row gutter={[16,16]}>
                 <Col lg={18} xs={24}>
                     <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${VideoDetail.filePath}`} controls></video>
 
-                        <List.Item actions={ [ <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/> ] }>
+                        {/* <List.Item actions={ [ <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/> ] }> */}
+                        <List.Item actions={ [ subscribeBtn ] }>
                             <List.Item.Meta
                                 avatar={<Avatar src={VideoDetail.writer && VideoDetail.writer.image} />}
                                 title={<a href="https://ant.design">{VideoDetail.title}</a>}
@@ -44,6 +49,7 @@ function VideoDetailPage(props) {
                         </List.Item>
 
                         {/* Comments */}
+                        <Comment />
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
